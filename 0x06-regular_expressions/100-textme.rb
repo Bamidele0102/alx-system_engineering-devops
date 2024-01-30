da67f6ee7b8bd3a7bae150a8089c4cd07c286295
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-01-30 22:20:07 +0100

    Textme

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,18 +1,4 @@
 #!/usr/bin/env ruby
-
-if ARGV.empty?
-	puts "Usage: #{$PROGRAM_NAME} {LOG_FILE_PATH}"
-	exit 1
-end
-
-FILE.foreach(ARGV[0]) do |line|
-	match_data = line.match(/\[from:(\S+)\] \[to:(\S+)\] \[flags:([^\]]+)\]/)
-
-	if mathc_data
-		sender = match_data[1]
-		receiver = match_data[2]
-		flags = match_data[3]
-
-		puts "#{sender},#{receiver},#{flags}"
-	end
-end
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
