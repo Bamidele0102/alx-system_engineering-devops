2220e9d7647e5d45b998b6975d87a1fe7f069093
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-05-07 22:44:56 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
-Query Reddit API and return the total number of subscribers
-for a given subreddit
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    get number of subscribers for a given subreddit
-    return 0 if invalid subreddit given
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
