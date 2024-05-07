4081e02c8a88afb8af0fb7a630a057de31ba696f
Idowu Olayiwola Bamidele <idowu.olayiwola.bamidele@gmail.com>
2024-05-07 23:40:30 +0100

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+returns the subscriber count of a given subreddit
 """
-
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
-        'AppleWebKit/537.36 (KHTML, like Gecko) '
-        'Chrome/58.0.3029.110 Safari/537.3'
-        }
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    try:
+        i = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                headers={'User-agent': 'hAxr'}, allow_redirects=False).json()
+        return i.get('data').get('subscribers') if not None else 0
+    except:
         return 0
